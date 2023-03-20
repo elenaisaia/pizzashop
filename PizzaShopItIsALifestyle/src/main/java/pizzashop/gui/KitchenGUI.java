4a34bb8c888ce76fa01054ebebb4f7e1fbe20974
elena_is02 <elenaisaia2002@gmail.com>
2023-03-21 00:19:09 +0200

    lab1 gata

--- PizzaShopItIsALifestyle/src/main/java/pizzashop/gui/KitchenGUI.java
+++ PizzaShopItIsALifestyle/src/main/java/pizzashop/gui/KitchenGUI.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class KitchenGUI {
-    public void KitchenGUI() {
+    public void kitchenGUIInit() {
         VBox vBoxKitchen = null;
 
         try {
@@ -29,13 +29,18 @@ public class KitchenGUI {
             public void handle(WindowEvent event) {
                 Alert exitAlert = new Alert(Alert.AlertType.CONFIRMATION, "Would you like to exit Kitchen window?", ButtonType.YES, ButtonType.NO);
                 Optional<ButtonType> result = exitAlert.showAndWait();
-                if (result.get() == ButtonType.YES){
-                    //Stage stage = (Stage) this.getScene().getWindow();
-                    stage.close();
-                }
-                // consume event
-                else if (result.get() == ButtonType.NO){
-                    event.consume();
+                if (result.isPresent()) {
+                    if (result.get() == ButtonType.YES){
+                        //Stage stage = (Stage) this.getScene().getWindow();
+                        stage.close();
+                    }
+                    // consume event
+                    else if (result.get() == ButtonType.NO){
+                        event.consume();
+                    }
+                    else {
+                        event.consume();
+                    }
                 }
                 else {
                     event.consume();
--- PizzaShopItIsALifestyle/src/main/java/pizzashop/gui/OrdersGUI.java
+++ PizzaShopItIsALifestyle/src/main/java/pizzashop/gui/OrdersGUI.java
@@ -1,11 +1,10 @@
 package pizzashop.gui;
 
-import javafx.event.EventHandler;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import pizzashop.controller.OrdersGUIController;
 import pizzashop.service.PizzaService;
 
@@ -37,13 +36,7 @@ public class OrdersGUI {
      stage.setTitle("Table"+getTableNumber()+" order form");
      stage.setResizable(false);
      // disable X on the window
-     stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-      @Override
-     public void handle(WindowEvent event) {
-         // consume event
-         event.consume();
-            }
-        });
+        stage.setOnCloseRequest(Event::consume);
      stage.setScene(new Scene(vBoxOrders));
      stage.show();
     }
